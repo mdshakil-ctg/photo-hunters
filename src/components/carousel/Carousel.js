fa6e48b0ff10c6d3d6b900e9bd3fd93e05e3bde0
shakil <mdshakil2898@gmail.com>
2022-11-08 01:30:15 +0600

    header nav bar & carousel added

---
+++ src/components/carousel/Carousel.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const Carousel = () => {
+   return (
+      <div className="carousel w-full">
+  <div id="slide1" className="carousel-item relative w-full">
+    <img src="https://i.ibb.co/LzkGmYM/carousel1.png" alt='carousel' className="w-full" />
+    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide4" className="btn btn-circle">❮</a> 
+      <a href="#slide2" className="btn btn-circle">❯</a>
+    </div>
+  </div> 
+  <div id="slide2" className="carousel-item relative w-full">
+    <img src="https://i.ibb.co/MGX9BB5/carousel2.png" alt='carousel' className="w-full" />
+    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide1" className="btn btn-circle">❮</a> 
+      <a href="#slide3" className="btn btn-circle">❯</a>
+    </div>
+  </div> 
+  <div id="slide3" className="carousel-item relative w-full">
+    <img src="https://i.ibb.co/DG62jwf/carousel3.png" alt='carousel' className="w-full" />
+    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide2" className="btn btn-circle">❮</a> 
+      <a href="#slide4" className="btn btn-circle">❯</a>
+    </div>
+  </div> 
+  <div id="slide4" className="carousel-item relative w-full">
+    <img src="https://i.ibb.co/dpHyGv4/carousel4.png" alt='carousel' className="w-full" />
+    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide3" className="btn btn-circle">❮</a> 
+      <a href="#slide1" className="btn btn-circle">❯</a>
+    </div>
+  </div>
+</div>
+   );
+};
+
+export default Carousel;
+
