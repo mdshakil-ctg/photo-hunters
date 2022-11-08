56d3e22ab77e92f373b1724eeb5c59321dd1468b
shakil <mdshakil2898@gmail.com>
2022-11-08 17:56:19 +0600

    about section added

---
+++ src/components/About/About.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+const About = () => {
+  return (
+   
+    <div className="flex flex-col lg:flex-row gap-5">
+      <div className="w-1/2">
+         <img src="https://i.ibb.co/whjrdZf/service5.png" alt=""></img>
+      </div>
+      <div className="w-1/2">
+      <h1>About Us</h1>
+      <h2 className="text-yellow-500">PHOTOGRAPHY IS GREAT</h2>
+      <p>
+        Photography is like a moment, an instant. You need a half-second to get
+        the photo. So it's good to capture people when they are themselves.I
+        became passionate about design filmmaking when I graduated from UCLA,
+        and one of the things I always wanted to do was shoot really high
+        quality film, so I got into time-lapse photography - so that means when
+        you shoot a flower.
+      </p>
+      <p>
+        You're shooting, like, one frame every twenty minutes, so that's <br /> Seconds
+        of a film per day. Photography is a pursuit that allows <br /> Hands-on with
+        what you show people of either yourself or the
+      </p>
+      <p>You do have a modicum of creative freedom as an actor, but you're still very much a cipher for other people's art. Photography gives you the opportunity to use your sensibility and everything you are to say something</p>
+    </div>
+    </div>
+  );
+};
+
+export default About;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,29 +1,34 @@
-import React from 'react';
-import Carousel from '../carousel/Carousel';
-import { useEffect, useState } from 'react';
-import HomeServices from '../homeServices/HomeServices';
-import { Link } from 'react-router-dom';
+import React from "react";
+import Carousel from "../carousel/Carousel";
+import { useEffect, useState } from "react";
+import HomeServices from "../homeServices/HomeServices";
+import { Link } from "react-router-dom";
+import About from "../About/About";
 
 const Home = () => {
-   const [services, setServices] = useState([]);
+  const [services, setServices] = useState([]);
 
-   useEffect(()=>{
-      fetch('http://localhost:5000/home/services')
-      .then(res=> res.json())
-      .then(data => setServices(data))
-   },[])
-   return (
-      <div>
-         <Carousel></Carousel>
-         <div className='grid lg:grid-cols-3 mt-24 mb-7'>
-            {
-               services.map(service => <HomeServices key={service._id} service={service}></HomeServices>)
-            }
-         </div>
-            <div className='text-center mb-7'><Link to='/services'><button className='btn btn-info'>See All</button></Link></div>
-         
+  useEffect(() => {
+    fetch("http://localhost:5000/home/services")
+      .then((res) => res.json())
+      .then((data) => setServices(data));
+  }, []);
+  return (
+    <div>
+      <Carousel></Carousel>
+      <div className="grid lg:grid-cols-3 mt-24 mb-7">
+        {services.map((service) => (
+          <HomeServices key={service._id} service={service}></HomeServices>
+        ))}
       </div>
-   );
+      <div className="text-center mb-7">
+        <Link to="/services">
+          <button className="btn btn-info">See All</button>
+        </Link>
+      </div>
+      <About></About>
+    </div>
+  );
 };
 
 export default Home;
